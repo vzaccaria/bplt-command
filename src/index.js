3bf241a37722616e9221516e94a6f53d723e6376
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-03-23 16:59:33 +0100

    Fix getOption

--- src/index.js
+++ src/index.js
@@ -7,16 +7,18 @@ var _ = require('lodash')
 var fs = require('fs')
 
 var getOption = (a, b, def, o) => {
-  if (_.isString(o[a])) {
+  "use strict"
+  if (!_.isUndefined(o[a])) {
     return o[a]
   } else {
-    if (_.isString(o[b])) {
+    if (!_.isUndefined(o[b])) {
       return o[b]
     } else {
       return def
     }
   }
 }
+
 
 
 var getOptions = doc => {
@@ -30,7 +32,9 @@ var getOptions = doc => {
 var doc = fs.readFileSync(__dirname + "/docs/usage.md", 'utf8')
 
 var main = () => {
-  var { help } = (getOptions(doc))
+  var {
+    help
+  } = (getOptions(doc))
 }
 
 main()
