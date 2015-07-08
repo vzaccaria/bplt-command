d230e1bf27504531123277370357bfd87a22f8b5
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2015-07-08 16:39:54 +0200

    feat: add git history to the workflow

--- configure.js
+++ configure.js
@@ -14,13 +14,18 @@ generateProject(_ => {
 
 
 	_.collect("docs", _ => {
-                _.cmd("./node_modules/.bin/mustache package.json docs/readme.md | ./node_modules/.bin/stupid-replace '~USAGE~' -f docs/usage.md > readme.md")
+		_.cmd("./node_modules/.bin/git-hist history.md")
+        _.cmd("./node_modules/.bin/mustache package.json docs/readme.md | ./node_modules/.bin/stupid-replace '~USAGE~' -f docs/usage.md > readme.md")
+        _.cmd("mkdir -p ./man")
+        _.cmd("pandoc -s -f markdown -t man readme.md > ./man/git-hist.1")
+        _.cmd("hub cm 'update docs and history.md'")
         })
 
   _.collectSeq("all", _ => {
     _.collect("build", _ => {
       _.babel("src/*.js")
       _.cmd("mkdir -p ./man")
+		_.cmd("make docs")
       _.cmd("pandoc -s -f markdown -t man readme.md > ./man/{{name}}.1")
     })
     _.cmd("((echo '#!/usr/bin/env node') && cat ./lib/index.js) > index.js", "./lib/index.js")
