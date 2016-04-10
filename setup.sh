e19ed55211d981fff04b5511a00cbcf02c71df17
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2016-04-10 13:32:57 +0200

    feat: make it work also for modules. no more need for bplt-module

--- setup.sh
+++ setup.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-./node_modules/.bin/babel configure.js | node
+./node_modules/.bin/babel --presets es2015,stage-2 configure.js | node
--- src/test.js
+++ src/test.js
@@ -1,12 +1,23 @@
 /*eslint quotes: [0] */
 
-var chai = require('chai')
-chai.use(require('chai-as-promised'))
-var should = chai.should()
+"use strict"
 
-var z		= require('zaccaria-cli')
+let chai = require('chai')
+chai.use(require('chai-as-promised'))
+let should = chai.should()
+let expect = chai.expect
+let _ = require('lodash')
+require('babel-polyfill')
+
+let debug = require('debug')
+let bluebird = require('bluebird')
+let shelljs = require('shelljs')
+let fs = require('fs')
+let mm = require('mm')
+
+
+var z = require('zaccaria-cli')
 var promise = z.$b
-var fs		= z.$fs
 
 /**
  * Promised version of shelljs exec
@@ -14,27 +25,54 @@ var fs		= z.$fs
  * @return {promise}     A promise for the command output
  */
 function exec(cmd) {
-  "use strict"
-  return new promise((resolve, reject) => {
-    require('shelljs').exec(cmd, {
-      async: true,
-      silent: true
-    }, (code, output) => {
-      if (code !== 0) {
-        reject(output)
-      } else {
-        resolve(output)
-      }
+    "use strict"
+    return new promise((resolve, reject) => {
+        require('shelljs').exec(cmd, {
+            async: true,
+            silent: true
+        }, (code, output) => {
+            if (code !== 0) {
+                reject(output)
+            } else {
+                resolve(output)
+            }
+        })
     })
-  })
 }
+
+mm.restore()
 
 /*global describe, it, before, beforeEach, after, afterEach */
 
 describe('#command', () => {
-  "use strict"
-  it('should show help', () => {
-    var usage = fs.readFileSync(`${__dirname}/../docs/usage.md`, 'utf8')
-    return exec(`${__dirname}/../index.js -h`).should.eventually.contain(usage)
-  })
+    "use strict"
+    it('should show help', () => {
+        var usage = fs.readFileSync(`${__dirname}/../docs/usage.md`, 'utf8')
+        return exec(`${__dirname}/../lib/index.js -h`).should.eventually.contain(usage)
+    })
+})
+
+describe('#module', () => {
+    let record = {}
+    let _module = require('./module');
+    _module = _module({
+        debug, _, shelljs, bluebird
+    })
+    "use strict"
+    it('should try to read help on readHelp', () => {
+
+        record = {}
+        mm(shelljs, 'exec', (c, opts, cb) => {
+            record.commandExecuted = c;
+            cb(0, 'file contents')
+        })
+
+        return _module.readHelp().then((v) => {
+
+            expect(record).to.contain({
+                commandExecuted: "cat ../docs/usage.md"
+            })
+            expect(v).to.deep.equal({code: 0, stdout: 'file contents'});
+        })
+    })
 })
